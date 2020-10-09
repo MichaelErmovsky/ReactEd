d07175a0963f0b74afd7ae6a5eb65e56db3c3146
Yaroslav <western633@gmail.com>
2020-10-09 14:50:42 +0300

    Added task

--- my-best-app/src/HelloWorld.js
+++ my-best-app/src/HelloWorld.js
@@ -1,8 +1,24 @@
 import React from 'react';
-const myName = () => {
-    return (
-      <span>Yarik</span>
-    );
+// const myName = () => {
+//     return (
+//       <span>Yarik</span>
+//     );
+// }
+
+// export default myName;
+
+class HelloWorld extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+       name: props.name 
+    };
+  }
+
+  render() {
+    return <span>{this.state.name}</span>
+  }
 }
 
-export default myName;
+export default HelloWorld;
