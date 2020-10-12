a336c79ad2a502f59702a2b45cd9faf72f85f36f
AndreyDatsenko <and@compent.net>
2020-10-12 14:42:33 +0300

    delete excess strukture

--- my-app/src/HelloWorld.js
+++ my-app/src/HelloWorld.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import Parent from './Parent';
 
 class Info extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            initText : 'Datsenko Andrey'
-        };
-    }
-
     render() {
-        const initText = this.state.initText;
-
         return (
             <div>
-                <Parent text={initText} />
+                {this.props.name}
             </div>
         );
     };
