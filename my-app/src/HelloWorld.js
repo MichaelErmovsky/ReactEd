5168f72fdee22c42e081f1fd3e5c8538d60bf6c3
AndreyDatsenko <and@compent.net>
2020-10-15 17:19:37 +0300

    custom input

--- my-app/src/HelloWorld.js
+++ my-app/src/HelloWorld.js
@@ -1,13 +1,38 @@
 import React from 'react';
 
 class Info extends React.Component {
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            value: this.props.data,
+            str: 'test'
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    static getDerivedStateFromProps(newProps, prevState) {
+        if (newProps.data !== prevState.value) {
+            return {
+                value: newProps.data
+            }
+        }
+        return null;
+    }
+
+    handleChange(event) {
+        let res = event.target.value.split(",");
+        this.props.onChange(res);
+    }
+
     render() {
         return (
-            <div>
-                {this.props.name}
-            </div>
+            <form>
+                <input type="text" value={this.state.value.join()} onChange={this.handleChange} />
+            </form>
         );
-    };
+    }
 }
 
 export default Info;
