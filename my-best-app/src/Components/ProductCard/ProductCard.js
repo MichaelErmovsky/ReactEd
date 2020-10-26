ce1c441bd67d9ed29efb101d87302221b845a8b0
Vadim <hedingoe@gmail.com>
2020-10-26 13:01:10 +0200

    init

---
+++ my-best-app/src/Components/ProductCard/ProductCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ProductCard.scss";
+
+class ProductCard extends React.Component {
+    render(){
+        return(
+            <div className="test">
+            d
+            </div>
+        )        
+    }
+}
+
+export default ProductCard;
