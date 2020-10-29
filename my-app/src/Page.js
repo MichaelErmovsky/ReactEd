71faba91f969fa266d955386d35421aff6504ea8
AndreyDatsenko <and@compent.net>
2020-10-29 13:32:22 +0200

    product list

--- my-app/src/Page.js
+++ my-app/src/Page.js
@@ -1,32 +1,13 @@
 import React from 'react';
-import ProductCard from './components/ProductCard';
+import ProductList from './components/ProductList';
 
 class Page extends React.Component{
 
-  state = {
-
-    product: {
-      id: 123456789,
-      name: 'Cookies',
-      description: 'Lorem ipsum dolor imet',
-      discount: '5%',
-      quantity: 10,
-      price: 45,
-      imageSrc: 'https://statically.io/images/misc/clouds.jpg'
-    }
-  };
-
   render (){
-    const item = this.state.product;
     return (
-      <ProductCard 
-      id = {item.id}
-      imgSrc = {item.imageSrc}
-      description = {item.description}
-      discount = {item.discount}
-      price = {item.price}
-      />
+      <ProductList/>
     );
   }
+
 }
 export default Page;
