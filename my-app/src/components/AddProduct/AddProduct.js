6e7d3ca49c8f64bd35f32de37124680d7f8715cb
AndreyDatsenko <and@compent.net>
2020-11-25 16:09:29 +0200

    add Clear & Submit functions for Add product component

--- my-app/src/components/AddProduct/AddProduct.js
+++ my-app/src/components/AddProduct/AddProduct.js
@@ -1,41 +1,102 @@
 import React from 'react';
 import './AddProduct.scss';
-//import {ReactComponent as Logo} from '../images/logo512.png'; for svg <Logo/>
 
 class AddProduct extends React.Component{
+  state = {
+    id: '',
+    name: '',
+    description: '',
+    discount: '',
+    quantity: '',
+    price: ''
+  };
+
+  handleClear(event){
+    event.preventDefault();
+    this.setState ({
+      id: '',
+      name: '',
+      description: '',
+      discount: '',
+      quantity: '',
+      price: ''
+    });
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+  
+  handleSubmit() {
+    //alert('name: ' + this.state.name);
+    if(!this.state.name){
+      let parent = document.getElementById('name').closest('.add-product__row');
+      parent.className += ' error';
+    }
+
+    if(!this.state.description){
+      let parent = document.getElementById('description').closest('.add-product__row');
+      parent.className += ' error';
+    }
+
+    if(!this.state.quantity){
+      let parent = document.getElementById('quantity').closest('.add-product__row');
+      parent.className += ' error';
+    }
+
+    if(!this.state.price){
+      let parent = document.getElementById('price').closest('.add-product__row');
+      parent.className += ' error';
+    }
+
+    this.setState({
+      id: Math.floor(Math.random() * (999999 - 1) + 999999),
+      name: this.state.name,
+      description: this.state.description,
+      discount: this.state.discount,
+      quantity: this.state.quantity,
+      price: this.state.price
+    });
+  }
 
   render (){
     return (
       <div className="add-product">
         <strong className="add-product__title">Add new product</strong>
+        {/* <div className={"add-product__row"+ (!this.state.name ? ' error' : '')}> */}
         <div className="add-product__row">
-          <label for="name" className="add-product__label">Add name<i className="add-product__marker"></i></label>
-          <input type="text" id="name"/>
-          <span className="add-product__error"></span>
+          <label htmlFor="name" className="add-product__label">Add name<i className="add-product__marker"></i></label>
+          <input type="text" id="name" value={this.state.name} onChange={this.handleChange.bind(this)}/>
+          <span className="add-product__error">Field is empty</span>
         </div>
         <div className="add-product__row">
-          <label for="description" className="add-product__label">Add description<i className="add-product__marker"></i></label>
-          <input type="text" id="description"/>
-          <span className="add-product__error"></span>
+          <label htmlFor="description" className="add-product__label">Add description<i className="add-product__marker"></i></label>
+          <input type="text" id="description" value={this.state.description} onChange={this.handleChange.bind(this)}/>
+          <span className="add-product__error">Field is empty</span>
         </div>
         <div className="add-product__row">
-          <label for="discount" className="add-product__label">Add discount</label>
-          <input type="text" id="discount"/>
+          <label htmlFor="discount" className="add-product__label">Add discount</label>
+          <input type="text" id="discount" value={this.state.discount} onChange={this.handleChange.bind(this)}/>
         </div>
         <div className="add-product__row">
-          <label for="quantity" className="add-product__label">Add quantity<i className="add-product__marker"></i></label>
-          <input type="text" id="quantity"/>
-          <span className="add-product__error"></span>
+          <label htmlFor="quantity" className="add-product__label">Add quantity<i className="add-product__marker"></i></label>
+          <input type="text" id="quantity" value={this.state.quantity} onChange={this.handleChange.bind(this)}/>
+          <span className="add-product__error">Field is empty</span>
         </div>
         <div className="add-product__row">
-          <label for="price" className="add-product__label">Add price<i className="add-product__marker"></i></label>
-          <input type="text" id="price"/>
-          <span className="add-product__error"></span>
+          <label htmlFor="price" className="add-product__label">Add price<i className="add-product__marker"></i></label>
+          <input type="text" id="price" value={this.state.price} onChange={this.handleChange.bind(this)}/>
+          <span className="add-product__error">Field is empty</span>
         </div>
         <div className="add-product__buttons">
-          <button className="add-product__btn">Create</button>
-          <button className="add-product__btn danger">Clear</button>
+          <button className="add-product__btn" onClick={this.handleSubmit.bind(this)}>Create</button>
+          <button className="add-product__btn danger" onClick={this.handleClear.bind(this)}>Clear</button>
         </div>
+        {<div>
+            {JSON.stringify(this.state)}
+          </div>}
       </div>
     );
   }
